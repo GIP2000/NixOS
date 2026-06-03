c890e6054c74a1b3d3dc6bab5db808e252517825
Gregory Presser <gpress2222@gmail.com>
2026-06-03 18:42:10 -0400

    added helium as my deafult browser so it stops opening firefox

--- home-manager/programs/browser.nix
+++ home-manager/programs/browser.nix
@@ -7,6 +7,17 @@
         inputs.helium.packages.x86_64-linux.helium-browser
     ];
 
+    xdg.mimeApps = {
+        enable = true;
+        defaultApplications = {
+            "text/html" = ["helium-browser.desktop"];
+            "x-scheme-handler/http" = ["helium-browser.desktop"];
+            "x-scheme-handler/https" = ["helium-browser.desktop"];
+            "x-scheme-handler/about" = ["helium-browser.desktop"];
+            "x-scheme-handler/unknown" = ["helium-browser.desktop"];
+        };
+    };
+
     programs = {
         # helium doesn't have a DRM yet so this is so I can watch netflix
         firefox = {
